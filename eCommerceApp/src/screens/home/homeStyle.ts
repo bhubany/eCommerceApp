48859d8204be9c59803216571e503f5171e09728
bhubany <yadav.bhuban.by@gmail.com>
2023-08-04 20:11:10 +0545

    Signed-off-by: Bhuban Yadav <bhubaneshwar.yadav@smaitic.com> feat(): Product card

--- eCommerceApp/src/screens/home/homeStyle.ts
+++ eCommerceApp/src/screens/home/homeStyle.ts
@@ -1,8 +1,21 @@
 import {View} from 'react-native';
 import styled from 'styled-components';
 
-export const HomeContainer = styled(View)``;
-export const HomeWrapper = styled(View)``;
-export const CarouselWrapper = styled(View)``;
-export const CircleWrapper = styled(View)``;
+export const HomeContainer = styled(View)`
+  width: 100%;
+`;
+export const HomeWrapper = styled(View)`
+  padding: 5px;
+`;
+export const CarouselWrapper = styled(View)`
+  margin-bottom: 10px;
+`;
+
+export const ProductCardWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
 export const CircleContainer = styled(View)``;
