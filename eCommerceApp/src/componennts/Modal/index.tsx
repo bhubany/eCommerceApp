d528aec0ee4475fb6498583bda99e5b14de12da1
bhubany <yadav.bhuban.by@gmail.com>
2023-08-19 00:12:28 +0545

    Signed-off-by: Bhuban Yadav <bhubaneshwar.yadav@smaitic.com> feat(): custom modal

--- eCommerceApp/src/componennts/Modal/index.tsx
+++ eCommerceApp/src/componennts/Modal/index.tsx
@@ -1,11 +1,35 @@
+import {COLORS} from 'common/enums';
+import {TextContent} from 'common/styles';
 import React from 'react';
-import {Text} from 'react-native';
+import {Button, Modal as DefaultModal} from 'react-native';
+import {ModalProps} from './modal';
 import MyModal from './modalStyle';
 
-const Modal = () => {
+const Modal = ({setOpen, children, ...props}: ModalProps) => {
   return (
     <MyModal.Container>
-      <Text>Modal</Text>
+      <DefaultModal transparent>
+        <MyModal.Wrapper>
+          <MyModal.Content>
+            <MyModal.Header>
+              {props.title && <TextContent>{props.title}</TextContent>}
+            </MyModal.Header>
+            <MyModal.Body>{children}</MyModal.Body>
+            <MyModal.Footer>
+              <Button
+                color={COLORS.DANGER}
+                title="Close"
+                onPress={() => setOpen(false)}
+              />
+              <Button
+                color={COLORS.SUCCESS}
+                title="Save"
+                onPress={props?.onPress}
+              />
+            </MyModal.Footer>
+          </MyModal.Content>
+        </MyModal.Wrapper>
+      </DefaultModal>
     </MyModal.Container>
   );
 };
--- eCommerceApp/src/componennts/Modal/modalStyle.ts
+++ eCommerceApp/src/componennts/Modal/modalStyle.ts
@@ -1,7 +1,51 @@
+import {COLORS} from 'common/enums';
 import {View} from 'react-native';
 import styled from 'styled-components';
 
-const Container = styled(View)``;
+const Container = styled(View)`
+  width: 100%;
+  height: 100%;
+  background-color: ${COLORS.WHITE};
+  opacity: 0.5;
+`;
+const Wrapper = styled(View)`
+  flex: 1;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
 
-const MyModal = {Container};
+const Content = styled(View)`
+  width: 90%;
+  background-color: ${COLORS.WHITE};
+  border-radius: 8px;
+  shadow-color: ${COLORS.BLACK};
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.25;
+  shadow-radius: 4px;
+  elevation: 5; //for android devices
+  padding: 10px;
+`;
+
+const Header = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  min-height: 20px;
+`;
+
+const Body = styled(View)`
+  padding: 10px;
+  align-items: center;
+`;
+
+const Footer = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const MyModal = {Container, Wrapper, Content, Header, Body, Footer};
 export default MyModal;
